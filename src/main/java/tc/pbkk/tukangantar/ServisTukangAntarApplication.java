8d8b748329ac3a4a074b6ac6f918668a5ec07192
Edwin <ykedwinh@gmail.com>
2019-05-02 17:18:42 +0700

    Create spring

---
+++ src/main/java/tc/pbkk/tukangantar/ServisTukangAntarApplication.java
@@ -0,0 +1,13 @@
+package tc.pbkk.tukangantar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServisTukangAntarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServisTukangAntarApplication.class, args);
+	}
+
+}
