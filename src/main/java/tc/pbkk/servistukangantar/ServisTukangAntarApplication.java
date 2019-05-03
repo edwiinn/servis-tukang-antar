a43a36f44fcbb1d0989d8b834c10e78ee854f821
Edwin <ykedwinh@gmail.com>
2019-05-03 09:30:47 +0700

    Handle Error in Spring

---
+++ src/main/java/tc/pbkk/servistukangantar/ServisTukangAntarApplication.java
@@ -0,0 +1,13 @@
+package tc.pbkk.servistukangantar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServisTukangAntarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServisTukangAntarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tc/pbkk/servistukangantar/ServisTukangAntarApplicationTests.java
@@ -0,0 +1,16 @@
+package tc.pbkk.servistukangantar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServisTukangAntarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
