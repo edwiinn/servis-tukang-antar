c619ddf8846512597542eabf95180d29a3cef59a
Ganendra Afrasya <gsculerlor@hotmail.com>
2019-05-07 09:45:41 +0700

    Model revision

--- src/main/java/tc/pbkk/servistukangantar/model/Delivery.java
+++ src/main/java/tc/pbkk/servistukangantar/model/Delivery.java
@@ -3,47 +3,79 @@ package tc.pbkk.servistukangantar.model;
 import javax.persistence.*;
 @Entity
 public class Delivery {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
-	private Integer id;
-	
+	private Integer id;	
 	private Integer orderId;
-	private Boolean isArrived;
+	private Integer driverId;
+	private String latestDriverLat;
+	private String latestDriverLong;
+	private String status;
 	private Long sentAt;
 	private Long arrivedAt;
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getOrderId() {
 		return orderId;
 	}
+
 	public void setOrderId(Integer orderId) {
 		this.orderId = orderId;
 	}
-	public Boolean getIsArrived() {
-		return isArrived;
+
+	public Integer getDriverId() {
+		return driverId;
 	}
-	public void setIsArrived(Boolean isArrived) {
-		this.isArrived = isArrived;
+
+	public void setDriverId(Integer driverId) {
+		this.driverId = driverId;
+	}
+
+	public String getLatestDriverLat() {
+		return latestDriverLat;
+	}
+
+	public void setLatestDriverLat(String latestDriverLat) {
+		this.latestDriverLat = latestDriverLat;
+	}
+
+	public String getLatestDriverLong() {
+		return latestDriverLong;
+	}
+
+	public void setLatestDriverLong(String latestDriverLong) {
+		this.latestDriverLong = latestDriverLong;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
 	}
 
 	public Long getSentAt() {
 		return sentAt;
 	}
-	
+
 	public void setSentAt(Long sentAt) {
 		this.sentAt = sentAt;
 	}
-	
+
 	public Long getArrivedAt() {
 		return arrivedAt;
 	}
-	
+
 	public void setArrivedAt(Long arrivedAt) {
 		this.arrivedAt = arrivedAt;
 	}
---
+++ src/main/java/tc/pbkk/servistukangantar/model/LocationLog.java
@@ -0,0 +1,60 @@
+package tc.pbkk.servistukangantar.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class LocationLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+    private Integer orderId;
+    private String driverLat;
+    private String driverLong;
+    private Long timestamp;
+
+    public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getDriverLat() {
+        return driverLat;
+    }
+
+    public void setDriverLat(String driverLat) {
+        this.driverLat = driverLat;
+    }
+
+    public String getDriverLong() {
+        return driverLong;
+    }
+
+    public void setDriverLong(String driverLong) {
+        this.driverLong = driverLong;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
