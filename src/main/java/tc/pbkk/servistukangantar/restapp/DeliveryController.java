2e0cf1828fe55a804bed84c7fa0c058995028f54
Edwin <ykedwinh@gmail.com>
2019-05-07 17:48:42 +0700

    Update All REST API to JSON Serealize

--- src/main/java/tc/pbkk/servistukangantar/restapp/DeliveryController.java
+++ src/main/java/tc/pbkk/servistukangantar/restapp/DeliveryController.java
@@ -25,38 +25,26 @@ import tc.pbkk.servistukangantar.utils.DependencyContainer;
 public class DeliveryController {
 	@Autowired
 	private DeliveryService deliveryService;
-	
 	private DependencyContainer dependencyContainer = DependencyContainer.getInstance();
+	private Gson gson = dependencyContainer.getService(Gson.class);
 	
-	@GetMapping()
-	public List<Delivery> getDeliveries() {
-		return deliveryService.getAllDelivery();
+	@GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String getDeliveries() {
+		return gson.toJson(deliveryService.getAllDelivery());
 	}
 	
-	@GetMapping("{id}")
-	public Delivery getDeliveryById(@PathVariable Integer id) {
-		return deliveryService.getDelivery(id);
+	@GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String getDeliveryById(@PathVariable Integer id) {
+		return gson.toJson(deliveryService.getDelivery(id));
 	}
 	
 	@PostMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public String postDelivery(@ModelAttribute Delivery delivery) {
-//		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-		Gson gson = dependencyContainer.getService(Gson.class);
 		return gson.toJson(deliveryService.addDelivery(delivery));
 	}
 	
-	@PutMapping("{id}")
-	public Delivery updateDelivery(@PathVariable Integer id, @ModelAttribute Delivery delivery) {
-		return deliveryService.updateDelivery(id, delivery);
+	@PutMapping(value = "{id}",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public String updateDelivery(@PathVariable Integer id, @ModelAttribute Delivery delivery) {
+		return gson.toJson(deliveryService.updateDelivery(id, delivery));
 	}
-	
-//	@DeleteMapping("{id}")
-//	public Map<String, String> deleteDelivery(@PathVariable Integer id) {
-//		deliveryService.deleteDelivery(id);
-//		
-//		Map<String, String > result = new LinkedHashMap<String, String>();
-//		result.put("result", "Delivery ID: " + id + " deleted!");
-//		
-//		return result;
-//	}
 }
