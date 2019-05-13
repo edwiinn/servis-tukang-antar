f63d8c77bdc4bb005dcd5552ba41ae8e4b2ef33f
Edwin <ykedwinh@gmail.com>
2019-05-13 14:25:33 +0700

    Service Structure

---
+++ src/main/java/tc/pbkk/servistukangantar/service/DeliveryLogService.java
@@ -0,0 +1,14 @@
+package tc.pbkk.servistukangantar.service;
+
+import java.util.List;
+
+import tc.pbkk.servistukangantar.model.LocationLog;
+
+public interface DeliveryLogService {
+	public List<LocationLog> getAllLogs();
+
+	public List<LocationLog> getLogsByDeliveryId(Integer deliveryId);
+	public LocationLog getLatestLogByDeliveryId(Integer deliveryId);
+	
+	public LocationLog addLog(LocationLog log);
+}
---
+++ src/main/java/tc/pbkk/servistukangantar/service/DeliveryLogServiceImpl.java
@@ -0,0 +1,33 @@
+package tc.pbkk.servistukangantar.service;
+
+import java.util.List;
+
+import tc.pbkk.servistukangantar.model.LocationLog;
+
+public class DeliveryLogServiceImpl implements DeliveryLogService {
+
+	@Override
+	public List<LocationLog> getAllLogs() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<LocationLog> getLogsByDeliveryId(Integer deliveryId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LocationLog getLatestLogByDeliveryId(Integer deliveryId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public LocationLog addLog(LocationLog log) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
